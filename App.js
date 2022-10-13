9ca06cd3fcd737f73ba6b15f095b611451c8e9f8
Davidon4 <Quayson450@gmail.com>
2022-10-13 02:04:22 +0100

    'save'

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 
-export default function App() {
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+const App = () => {
+  const [isPickerShow, setIsPickerShow] = useState(false);
+  const [time, setTime] = useState(new Date(Date.now()));
+
+  const options = { hour: "2-digit", minute: "2-digit" };
+
+  const showPicker = () => {
+    setIsPickerShow(true);
+  };
+
+  const hidePicker = () => {
+    setIsPickerShow(false);
+  };
+
+  const onChange = (event, value) => {
+    setTime(value);
+    if (Platform.OS === "android") {
+      setIsPickerShow(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Display the selected date */}
+      <TouchableOpacity
+        onPress={!isPickerShow ? showPicker : hidePicker}
+        style={styles.pickedDateContainer}
+      >
+        <Text style={styles.pickedDate}>
+          {time.toLocaleTimeString([], options)}
+        </Text>
+      </TouchableOpacity>
+
+      {/* The date picker */}
+      {isPickerShow && (
+        <DateTimePicker
+          value={time}
+          mode={"time"}
+          display={"spinner"}
+          onChange={onChange}
+          textColor="#CE2EBE"
+          style={styles.datePicker}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 50,
+  },
+  pickedDateContainer: {
+    padding: 20,
+    backgroundColor: "#eee",
+    borderRadius: 10,
+  },
+  pickedDate: {
+    fontSize: 18,
+    color: "#CE2EBE",
+  },
+  btnContainer: {
+    padding: 30,
+  },
+  // This only works on iOS
+  datePicker: {
+    width: 320,
+    height: 260,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
   },
 });
+
+export default App;
